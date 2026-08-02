5557735c8f6ce0ba676ef98d334ba0076f59bdca
Brian Burton <brian@burton-computer.com>
2026-08-02 13:29:33 -0400

    entrypoint script second argument is optional

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,20 +2,39 @@
 
 set -e
 
-dir="$1"
-file="$2"
-shift 2
+if [[ $# -lt 1 ]] ; then
+  echo "error: at least one argument required (directory)" 1>&2
+  exit 1
+fi
 
-if [[ "$dir" == "/"* ]] ; then
-  if [[ ! -d "$dir" ]] ; then
-    echo error: no such directory: "$dir" 1>&2
-    exit 1
-  fi
-else
-  echo error: first arg must be an absolute directory path 1>&2
+dir="$1"
+
+if [[ "$dir" != "/"* ]] ; then
+  echo "error: first arg must be an absolute directory path" 1>&2
+  exit 1
+fi
+
+if [[ ! -d "$dir" ]] ; then
+  echo "error: no such directory: $dir" 1>&2
   exit 1
 fi
 
 cd "$dir"
 export XDG_CONFIG_HOME=/config
-exec emacs -nw "$file" "$@"
+
+# If only 1 argument, run emacs without a file
+# If 2+ arguments, second is the file, remaining pass to emacs
+if [[ $# -eq 1 ]] ; then
+  exec emacs -nw
+else
+  shift
+  file="$1"
+  shift
+  
+  if [[ -f "$file" ]] ; then
+    exec emacs -nw "$file" "$@"
+  else
+    echo "error: no such file: $file" 1>&2
+    exit 1
+  fi
+fi
