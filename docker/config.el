b4d24e0e1708e29dddd8edd7587e559d9654bf8d
Brian Burton <brian@burton-computer.com>
2026-08-01 22:58:19 -0400

    add org-roam

--- docker/config.el
+++ docker/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -42,6 +42,11 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; org-roam has its own directory
+;; https://www.orgroam.com/manual.html
+(setq org-roam-directory (file-truename "~/org-roam"))
+(after! org
+  (org-roam-db-autosync-mode))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
